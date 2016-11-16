1f6f151b7a144e57345ab2bf454d352eb2be44b7
Nelson Chen <nelsonchen1020@gmail.com>
2016-11-16 11:09:47 -0800

    Buy & Sell actions implemented for first version

--- js/main.js
+++ js/main.js
@@ -1,6 +1,46 @@
 $(document).ready( function(){
   console.log('ehllo')
 
+  //Select Buy
+  $(".buy").click(function() {
+    console.log(this)
+    $(this).css("background-color", "green")
+
+    var a = $(this).children()
+    $(a).css("color", "white")
+
+    var sell = $(this).siblings()[1]
+    $(sell).children().css("color", "white")
+    $(sell).children().html("BUY")
+    $(sell).css("background-color", "green")
+
+    var outer = $(this).parent()
+    var inner = $(this).next()
+    $(outer).css("background", "white")
+    $(outer).css("border", "5px solid green")
+    $(inner).children().css("color", "green")
+  })
+
+  //Select Sell
+  $(".sell").click(function() {
+    console.log(this)
+    $(this).css("background-color", "red")
+
+    var a = $(this).children()
+    $(a).css("color", "white")
+
+    var sell = $(this).siblings()[0]
+    $(sell).children().css("color", "white")
+    $(sell).children().html("SELL")
+    $(sell).css("background-color", "red")
+
+    var outer = $(this).parent()
+    var inner = $(this).prev()
+    $(outer).css("background", "white")
+    $(outer).css("border", "5px solid red")
+    $(inner).children().css("color", "red")
+  })
+
   var shine = new Shine(document.getElementById('shine'));
 
   console.log('shine')
@@ -11,7 +51,7 @@ $(document).ready( function(){
     shine.draw();
   }, false);
 
-  window.addEventListener('mousemove', handleMouseMove, false);
+
 
   // function update() {
   //   window.requestAnimationFrame(update);
