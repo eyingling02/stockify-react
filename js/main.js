9be29d5d8420ae1d258d77aca9e2f59df3febef8
Nelson Chen <nelsonchen1020@gmail.com>
2016-11-15 22:10:15 -0800

    Bug fixing shine with Vic & Erica

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
-
-
 $(document).ready( function(){
-
   console.log('ehllo')
 
-  $("div.outer").click(function(){
-    console.log('outer')
-    $(this).blur(function(){
-      alert()
-  })
-  })
+  var shine = new Shine(document.getElementById('daily_pick'));
+
+  console.log('shine')
+
+  window.addEventListener('mousemove', function(event) {
+    shine.light.position.x = event.clientX;
+    shine.light.position.y = event.clientY;
+    shine.draw();
+  }, false);
 })
+
+//   $("div.outer").on("click", function(){
+//     $("div.outer").blur()
+//     console.log('outer')
+//     console.log(this)
+//     // $(this).on("blur", function() {
+//     //   alert("hi")
+//     // })
+//   })
+// })
 
 
     // $($(this).parent()["0"].firstChild.nextElementSibling).one(replaceWith($("div.inner h2"))
