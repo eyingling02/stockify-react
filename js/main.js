29a731fef5bb8b6cd41e3953359d5f85ca99ab3f
Nelson Chen <nelsonchen1020@gmail.com>
2016-11-16 09:59:03 -0800

    Implemented shine, but the background color does not match.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready( function(){
   console.log('ehllo')
 
-  var shine = new Shine(document.getElementById('daily_pick'));
+  var shine = new Shine(document.getElementById('shine'));
 
   console.log('shine')
 
@@ -10,17 +10,23 @@ $(document).ready( function(){
     shine.light.position.y = event.clientY;
     shine.draw();
   }, false);
-})
 
-//   $("div.outer").on("click", function(){
-//     $("div.outer").blur()
-//     console.log('outer')
-//     console.log(this)
-//     // $(this).on("blur", function() {
-//     //   alert("hi")
-//     // })
-//   })
-// })
+  window.addEventListener('mousemove', handleMouseMove, false);
+
+  // function update() {
+  //   window.requestAnimationFrame(update);
+  //   var time = new Date().getTime();
+  //   var speed = 0.00025;  // 1 = 1000 rotations/s
+  //   var phase = time * speed * 2.0 * Math.PI;
+  //   var radiusX = window.innerWidth * 0.5;
+  //   var radiusY = window.innerHeight * 0.5;
+  //   shine.light.position.x = radiusX + radiusX * Math.cos(phase);
+  //   shine.light.position.y = radiusY + radiusY * Math.sin(phase * 0.7);
+  //   shine.draw();
+  // }
+  //
+  // update();
+})
 
 
     // $($(this).parent()["0"].firstChild.nextElementSibling).one(replaceWith($("div.inner h2"))
