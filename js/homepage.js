9176c21cbacae2e11b8fa344411b499868427bea
Nelson Chen <nelsonchen1020@gmail.com>
2016-11-24 19:02:27 -0800

    starting nflx tile layout

---
+++ js/homepage.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  // $('.favorites').slick({
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   fade: true,
+  //   cssEase: 'linear'
+  // });
+  $('.favorites').slick({
+  infinite: true,
+  slidesToShow: ,
+  slidesToScroll: 3
+  });
+
+});
