cc9894029aeadeb6af0daa34f68b7b99882a6a32
Nelson Chen <nelsonchen1020@gmail.com>
2016-11-29 15:52:43 -0800

    Buy / Sell actions turn background black rather than white

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 $(document).ready( function(){
   //Select Buy
   $(".buy").click(function() {
-    $(this).css("background-color", "green")
+    // $(this).css("background-color", "green")
+    $(this).css("background-color", "darkgreen")
 
     var a = $(this).children()
     $(a).css("color", "white")
     var sell = $(this).siblings()[1]
-    // $(sell).children().css("color", "white")
     $(sell).children().css("color", "white")
     $(sell).children().html("BUY")
-    $(sell).css("background-color", "green")
+    // $(sell).css("background-color", "green")
+    $(sell).css("background-color", "darkgreen")
 
     var outer = $(this).parent()
     var inner = $(this).next()
-    $(outer).css("background", "white")
-    $(outer).css("border", "5px solid green")
-    // $(inner).children().css("color", "green")
-    $(inner).children().css("color", "#c5b358")
+    // $(outer).css("background", "white")
+    $(outer).css("background", "black")
+    // $(outer).css("border", "5px solid green")
+    $(outer).css("border", "5px solid darkgreen")
+    $(inner).children().css("color", "white")
+    // $(inner).children().css("color", "#c5b358")
   })
 
   //Select Sell
@@ -33,10 +36,11 @@ $(document).ready( function(){
 
     var outer = $(this).parent()
     var inner = $(this).prev()
-    $(outer).css("background", "white")
+    // $(outer).css("background", "white")
+    $(outer).css("background", "black")
     $(outer).css("border", "5px solid red")
-    // $(inner).children().css("color", "red")
-    $(inner).children().css("color", "#c5b358")
+    $(inner).children().css("color", "white")
+    // $(inner).children().css("color", "#c5b358")
   })
 
   var shine = new Shine(document.getElementById('shine'));
