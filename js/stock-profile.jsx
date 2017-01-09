4537bb7c3c1deabedb35e00e74a08bbed219ce9d
Nelson Chen <nelsonchen1020@gmail.com>
2017-01-09 12:21:16 -0800

    Add sub sections divs

--- js/stock-profile.jsx
+++ js/stock-profile.jsx
@@ -5,7 +5,7 @@ const Topline = require('./stock-topline')
 const Chart = require('./stock-chart')
 const ChartPrediction = require('./stock-chart-prediction')
 const Filters = require('./stock-filter-big-name')
-const Sorting = require('./stock-sort-little-text')
+const Sorting = require('./stock-sort-user-text')
 const OpinionsPredictions = require('./stock-opinions')
 
 const data = require('../data')
@@ -14,17 +14,19 @@ class Stock extends React.Component {
     render() {
         return (
           <div className="individual-stock">
-            <div className='stock-top-section'>
-                <div className='personal'><Personal /></div>
-                <div className='topstats'><TopStats /></div>
-                <div className='gotostock'><GoToUser /></div>
+            <div className='stock-top-stock-section'>
+                <div className='topline'><Topline /></div>
             </div>
-            <div className='stock-mid-section'>
-                <div className='nextupcoming'><NextUpcoming /></div>
-                <div className='personalbest'><PersonalBest /></div>
+            <div className='stock-main-chart-section'>
+                <div className='chart'><Chart /></div>
+                <div className='chart-prediction'><ChartPrediction /></div>
             </div>
-            <div className='stock-bottom-section'>
-                <div className='stockportfolio'><UserPortfolio /></div>
+            <div className='stock-mid-filter-section'>
+                <div className='stock-filters'><Filters /></div>
+            </div>
+            <div className='stock-bottom-text-section'>
+                <div className='sort-user-text'><Sorting /></div>
+                <div className='user-text'><OpinionsPredictions /></div>
             </div>
           </div>
         )
